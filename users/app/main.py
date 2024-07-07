7ac9279b85207c3cce99031ec1db42eaf79e4492
zacksatylgan <166334246+zacksatylgan@users.noreply.github.com>
2024-07-07 17:02:42 -0500

    add: changes

--- users/app/main.py
+++ users/app/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, Request, Response
+from fastapi import FastAPI, Request, Response, HTTPException, Query
 from fastapi.templating import Jinja2Templates
 
 from .users import create_users
@@ -15,7 +15,27 @@ def index(request: Request):
 
 
 # (сюда писать решение)
-
-
-
 # (конец решения)
+
+@app.get("/users")
+def get_users(request: Request, page: int = 1, per_page: int = 10):
+    start = (page - 1) * per_page
+    end = start + per_page
+    paginated_users = users[start:end]
+    total_pages = (len(users) + per_page - 1) // per_page  # Вычисляем общее количество страниц
+    return templates.TemplateResponse(
+        "users.html",
+        {
+            "request": request,
+            "users": paginated_users,
+            "page": page,
+            "total_pages": total_pages
+        }
+    )
+
+@app.get("/users/{id}")
+def get_user(request: Request, id: int):
+    user = next((user for user in users if user["id"] == id), None)
+    if user is None:
+        raise HTTPException(status_code=404, detail="User not found")
+    return templates.TemplateResponse("user.html", {"request": request, "user": user})
